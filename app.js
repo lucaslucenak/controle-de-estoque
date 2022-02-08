bcafc0319f30b2882f3aca002b7ec4e6840a9438
lucaslucenak <lucas.lucenak@gmail.com>
2022-02-08 11:27:34 -0300

    bug fix, body parser.urlencoded({extended: true})

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const db = require('./models/db');
 
     //BodyParser
     const bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
 
